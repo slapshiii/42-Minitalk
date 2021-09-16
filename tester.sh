d356810917c3a39a355f3f51237971247f271501
phnguyen <phnguyen@student.42.fr>
2021-09-16 22:05:55 +0200

    improve tester

--- tester.sh
+++ tester.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-for i in `seq 1 200`; do
-	./client $1 "$(cat -e Makefile)"
+#for i in `seq 1 5000`; do
+#	./client $1 "test"
+#	echo "req $i"
+#done
+
+#for i in `seq 1 100`; do
+#	./client $1 "$(cat -e Makefile)"
+#	echo "req $i"
+#done
+
+#for i in `seq 1 5000`; do
+#	./client $1 💯
+#	echo "req $i"
+#done
+
+for i in `seq 1 5000`; do
+	./client $1 𐏀𐎬𐏂
 	echo "req $i"
 done
